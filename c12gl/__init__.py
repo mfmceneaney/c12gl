86dc4df66ea35b25d8343393205df82bbd3cd79d
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-09-07 11:48:14 -0400

    Added import statements to `c12gl/__init__.py` to get API reference to show.

--- c12gl/__init__.py
+++ c12gl/__init__.py
@@ -3,3 +3,10 @@ C12GL - Python library for Graph Learning oriented toward CLAS12 tasks
 """
 
 __version__ = '0.1.0'
+
+#NOTE: Imports necessary for API documentation to show.
+from .dataloading import *
+from .models import *
+from .optimization import *
+from .preprocessing import *
+from .utils import *
